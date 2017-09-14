e3c405366ec6c680dd5f616bd029e2a769919783
web_liuyaowei <web_liuyaowei@sina.com>
2017-09-14 17:44:08 +0800

    最新权限

---
+++ ERPvueiView/src/components/Customer/number.vue
@@ -0,0 +1,26 @@
+<template>
+    <input type="text" @keydown="KeydownNumber" />
+</template>
+
+<script>
+export default {
+    name: 'hello',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    },
+    KeydownNumber() {
+        let code = event.keyCode
+        if (!(code >= 48 && code <= 57) && !(code >= 96 && code <= 105) && code != 110 && code != 190 && code != 8 && code != 37 && code != 39) {
+            event.returnValue = false
+        } else {
+            event.returnValue = true
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ OrderSystem/.svn/pristine/46/4625811e801617cc3f49d8332067934316c6f244.svn-base
@@ -0,0 +1,27 @@
+<template>
+   <div class="NotAuthority">
+       <div class="text">暂无权限</div>
+   </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style lang='stylus'>
+  .NotAuthority
+     height: 100%
+     position: relative
+     .text
+        position: absolute
+        top: 30%
+        left: 50%
+        transform: translate(-50%,-50%)
+        color: #8492A6;
+        font-size: 50px;
+</style>
+
